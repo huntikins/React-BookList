c81debb723fb481027769fab58e83b17c34c7695
htrammell <hunterttrammell@gmail.com>
2019-08-10 14:36:25 -0500

    file push

---
+++ client/build/precache-manifest.60e74fbd2c3e2c2f1338184df8ad71a5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "6fcda2698a4aa0796708",
+    "url": "/static/js/main.6fcda269.chunk.js"
+  },
+  {
+    "revision": "16693711b03cdb27b87a",
+    "url": "/static/js/1.16693711.chunk.js"
+  },
+  {
+    "revision": "6fcda2698a4aa0796708",
+    "url": "/static/css/main.3aca59a0.chunk.css"
+  },
+  {
+    "revision": "a9ef0b98c693f699fac281ed50f71892",
+    "url": "/index.html"
+  }
+];
